0aaab85826d0089744fee255fda9fd93e819bd85
andersonluix <109183419+andersonluix@users.noreply.github.com>
2022-11-03 20:27:38 -0300

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-1lq9ggn2cscw6sd5qjgzzg8bmr8rea2glc7dr9p4z3qoqknq63-6oct4p9hdy7qdq57r8dk3dze3 10.10.10.100:2377
+docker swarm join --token SWMTKN-1-1lq9ggn2cscw6sd5qjgzzg8bmr8rea2glc7dr9p4z3qoqkyq63-6oct4p9hdy7qdq57r8dk3dze3 10.10.10.100:2377
